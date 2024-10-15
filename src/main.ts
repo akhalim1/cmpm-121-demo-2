fe792ca07d2908eaf527a6632eb4559ceac0ea83
Alexander Halim <akhalim@ucsc.edu>
2024-10-15 11:33:34 -0700

    title change

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "My Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
